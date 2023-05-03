c7e9dcaa3ced5ec36e67e67584f983b5dfd808d2
ilyadudkin <iluha805@gmail.com>
2023-05-03 11:39:43 +0300

    getSummationAge теперь метод класс Animal

--- Animals.js
+++ Animals.js
@@ -3,6 +3,16 @@ class Animal{
         this.name = name;
         this.age = age;
         this.childs = childs;
+        this.getSummationAge = function (){
+            let sum = 0
+            sum += this.age
+            if (childs!=null){
+                for (let i in childs){
+                sum += childs[i].getSummationAge()
+                }
+            }
+            return sum
+        }
     }
 }
 class Cat extends Animal{
@@ -20,18 +30,6 @@ class Cow extends Animal{
         super(name,age,childs)
     }
 }
-function getSummationAge(animals){
-    let sum = 0
-    for (let i in animals){
-        sum += animals[i].age
-        childSum = 0
-        if (animals[i].childs!=null){
-            childSum+=getSummationAge(animals[i].childs)
-        }
-        sum += childSum
-    }
-    return sum
-}
 function clone(object){
     let new_object = new Animal(object.name,object.age,object.childs)
     return new_object
@@ -39,6 +37,4 @@ function clone(object){
 cats = [new Cat("Joe",5,[new Cat("Child1",3) ,new Cat("Child2",5)]), new Cat("Silvia",4)]
 dogs_wtf = clone(cats[0])
 dogs_wtf.name = 'Brian'
-console.log(dogs_wtf)
-console.log(cats[0])
-console.log(getSummationAge(cats))
+console.log(dogs_wtf.getSummationAge())
